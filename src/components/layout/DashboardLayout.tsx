3dc068b2cfe3a63196930820cc0b8b72768565a8
Anish Jain <43033153+itsanishjain@users.noreply.github.com>
2025-03-29 14:30:53 +0530

    fixxxx zittering

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,6 +1,4 @@
-
 import React from "react";
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import DashboardSidebar from "./DashboardSidebar";
 
 interface DashboardLayoutProps {
@@ -9,18 +7,19 @@ interface DashboardLayoutProps {
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
-    <SidebarProvider>
+    <>
       <div className="flex h-screen overflow-hidden bg-background">
+        {/* Sidebar - hidden on mobile, visible on desktop */}
         <DashboardSidebar />
-        
+
         {/* Main content area */}
-        <div className="flex flex-col flex-1 w-full overflow-hidden">
+        <div className="flex flex-col flex-1 w-0 overflow-hidden">
           <main className="relative flex-1 overflow-y-auto focus:outline-none p-4 md:p-6">
             {children}
           </main>
         </div>
       </div>
-    </SidebarProvider>
+    </>
   );
 };
 
